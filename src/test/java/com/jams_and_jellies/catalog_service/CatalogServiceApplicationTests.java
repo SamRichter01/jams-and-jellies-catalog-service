1aac98f8b4a644327e88170f0570172d1d0039f4
RICH9794 <rich9794@stthomas.edu>
2025-02-25 11:33:29 -0600

    Initial Commit

---
+++ src/test/java/com/jams_and_jellies/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jams_and_jellies.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
